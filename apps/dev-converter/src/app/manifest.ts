acb001de58971eb0dabef95eb8c54ec8d568463d
eliyahu-kriel <44751346+elik56a@users.noreply.github.com>
2026-01-07 09:08:31 +0200

    Update manifest.ts

--- apps/dev-converter/src/app/manifest.ts
+++ apps/dev-converter/src/app/manifest.ts
@@ -15,12 +15,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon",
         sizes: "32x32",
         type: "image/png",
+        purpose: "any",
       },
       {
         src: "/apple-icon",
         sizes: "180x180",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
     ],
   }
